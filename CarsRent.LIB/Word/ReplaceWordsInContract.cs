7997cbb006b10a6c9939609ade97ee0792ee0bbc
Maxim Babakov <mister.franz02@gmail.com>
2022-07-29 11:02:05 +0700

    Созданные документы Word теперь сортируются по подпапкам.

--- CarsRent.LIB/Word/ReplaceWordsInContract.cs
+++ CarsRent.LIB/Word/ReplaceWordsInContract.cs
@@ -8,14 +8,20 @@ namespace CarsRent.LIB.Word
     {
         public void Replace(ContractDetails contract)
         {
-            var documentName = $"{contract.ConclusionDate} {contract.Car.Color} {contract.Car.Brand} {contract.Car.Model}" +
-                $" {contract.Renter.Surname} {contract.Renter.Name[0]}.{contract.Renter.Patronymic[0]}.";
             var settingsSerializator = new SettingsSerializator<TemplatesSettings>();
             var settings = settingsSerializator.Deserialize();
+            var outputFolder = Path.Combine(settings.OutputFolder, $"{contract.Car.Color} {contract.Car.Brand} {contract.Car.Model}");
+
+            var documentName = $"{contract.ConclusionDate} {contract.Renter.Surname} {contract.Renter.Name[0]}.{contract.Renter.Patronymic[0]}.";
 
             var replaceWordsGenerator = new ReplaceWordsGenerator(settings, contract);
-            var documentReplacer = new DocumentReplacer(settings.ContractSample, settings.OutputFolder);
-            documentReplacer.Replace(replaceWordsGenerator.GetWords(), documentName);
+
+            var documentReplacer = new DocumentReplacer(settings.ContractSample, outputFolder);
+            documentReplacer.Replace(replaceWordsGenerator.GetWords(), $"{documentName} договор");
+            documentReplacer = new DocumentReplacer(settings.ActSample, outputFolder);
+            documentReplacer.Replace(replaceWordsGenerator.GetWords(), $"{documentName} акт");
+            documentReplacer = new DocumentReplacer(settings.NotificationSample, outputFolder);
+            documentReplacer.Replace(replaceWordsGenerator.GetWords(), $"{documentName} уведомление");
         }
     }
 }
