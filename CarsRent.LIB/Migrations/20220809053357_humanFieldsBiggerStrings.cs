fe6a3e62757c33e8a1b09888f8122d8f751360d2
Maxim Babakov <mister.franz02@gmail.com>
2022-08-09 12:36:06 +0700

    Some human string fields gets bigger max length.

---
+++ CarsRent.LIB/Migrations/20220809053357_humanFieldsBiggerStrings.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarsRent.LIB.Migrations
+{
+    public partial class humanFieldsBiggerStrings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "Humans",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RegistrationPlace",
+                table: "Humans",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Patronymic",
+                table: "Humans",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Humans",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IssuingOrganization",
+                table: "Humans",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "Humans",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RegistrationPlace",
+                table: "Humans",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Patronymic",
+                table: "Humans",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Humans",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IssuingOrganization",
+                table: "Humans",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+    }
+}
--- CarsRent.LIB/Model/Human.cs
+++ CarsRent.LIB/Model/Human.cs
@@ -10,13 +10,13 @@ namespace CarsRent.LIB.Model
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Вы не ввели имя")]
-        [StringLength(20, MinimumLength = 1, ErrorMessage = "Длина имени не должна быть меньше 1 символа и превышать 20 символов.")]
+        [StringLength(30, MinimumLength = 1, ErrorMessage = "Длина имени не должна быть меньше 1 символа и превышать 20 символов.")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Вы не ввели фамилию")]
-        [StringLength(20, MinimumLength = 1, ErrorMessage = "Длина фамилии не должна быть меньше 1 символа и превышать 20 символов.")]
+        [StringLength(30, MinimumLength = 1, ErrorMessage = "Длина фамилии не должна быть меньше 1 символа и превышать 20 символов.")]
         public string Surname { get; set; }
         [Required(ErrorMessage = "Вы не ввели отчество")]
-        [StringLength(20, MinimumLength = 1, ErrorMessage = "Длина отчества не должна быть меньше 1 символа и превышать 20 символов.")]
+        [StringLength(30, MinimumLength = 1, ErrorMessage = "Длина отчества не должна быть меньше 1 символа и превышать 20 символов.")]
         public string Patronymic { get; set; }
         [Required(ErrorMessage = "Вы не ввели дату рождения")]
         [Date(ErrorMessage = "Некорректная дата рождения")]
@@ -28,13 +28,13 @@ namespace CarsRent.LIB.Model
         [PassportNumber(ErrorMessage = "Некорректно введены серия/номер паспорта")]
         public string IdentityNumber { get; set; }
         [Required(ErrorMessage = "Вы не ввели кем выдан паспорт")]
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "Длинна строки \"кем выдан паспорт\" должна быть не меньше 3 и не больше 50 символов")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Длинна строки \"кем выдан паспорт\" должна быть не меньше 3 и не больше 200 символов")]
         public string IssuingOrganization { get; set; }
         [Required(ErrorMessage = "Не введена дата выдачи паспорта")]
         [Date(ErrorMessage = "Некорректная дата выдачи паспорта")]
         public string IssuingDate { get; set; }
         [Required(ErrorMessage = "Вы не ввели место регистрации")]
-        [StringLength(100, MinimumLength = 3, ErrorMessage = "Длина строки \"место регистрации\" должна быть не меньше 3 и не больше 50 символов")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Длина строки \"место регистрации\" должна быть не меньше 3 и не больше 200 символов")]
         public string RegistrationPlace { get; set; }
 
         [XmlIgnore]
