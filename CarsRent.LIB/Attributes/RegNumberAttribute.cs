9ba7e501ce9188082c8f3e2b4a90250736f20884
sad_degenerate <mister.franz02@gmail.com>
2022-12-10 11:09:44 +0700

    Attributes refactoring.

--- CarsRent.LIB/Attributes/RegNumberAttribute.cs
+++ CarsRent.LIB/Attributes/RegNumberAttribute.cs
@@ -5,16 +5,11 @@ namespace CarsRent.LIB.Attributes;
 
 public class RegNumberAttribute : ValidationAttribute
 {
+    private const string Pattern = @"[АВЕКМНОРСТУХ]\d{3}[АВЕКМНОРСТУХ]{2}\d{2,3}$";
+    
     public override bool IsValid(object? value)
     {
         var str = value?.ToString();
-        if (string.IsNullOrWhiteSpace(str))
-        {
-            return false;
-        }
-
-        const string pattern = @"[АВЕКМНОРСТУХ]\d{3}[АВЕКМНОРСТУХ]{2}\d{2,3}$";
-
-        return Regex.IsMatch(str, pattern, RegexOptions.IgnoreCase);
+        return string.IsNullOrWhiteSpace(str) == false && Regex.IsMatch(str, Pattern, RegexOptions.IgnoreCase);
     }
 }
